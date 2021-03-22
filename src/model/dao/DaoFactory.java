4e7e5ffb067754cbc756ebcab8f77866d8d5e147
Carlos Augusto <augustino2003@hotmail.com>
2021-03-22 19:21:19 -0300

    findByDepartment implementation

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -8,5 +8,9 @@ public class DaoFactory {
 	public static SellerDao createSellerDao() {
 		return new SellerDaoJDBC(DB.getConnection());
 	}
+	
+//	public static DepartmentDao createDepartmentDao() {
+//		return new DepartmentDaoJDBC(DB.getConnection());
+//	}
 
 }
